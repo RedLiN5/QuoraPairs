c6c8bbe6214d77e010c0402658ef1e2bc0e5d93a
Leslie_Yan <leslieyan0621@gmail.com>
2017-04-20 17:24:44 +0800

    [Feature] EDA

--- quorapairs.py
+++ quorapairs.py
@@ -1,3 +1,15 @@
 import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+import xgboost as xgb
+
 
 df = pd.read_table('train.csv', header=0, index_col=0, sep=',')
+test = pd.read_table('/Users/Leslie/GitHub/QuoraPairs/test.csv', header=0, index_col=None, sep=',')
+
+df = df.fillna("")
+test = test.fillna("")
+
+df1 = df[['qid1', 'question1']]
+df2 = df[['qid2', 'question2']]
+
